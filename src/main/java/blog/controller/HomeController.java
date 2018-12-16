c9ce93950e5960b90d9003cab4c5a29f799d182a
Egor <egor.litvinchuk@grizzly.by>
2018-12-16 18:47:57 +0300

    transfer

--- src/main/java/blog/controller/HomeController.java
+++ src/main/java/blog/controller/HomeController.java
@@ -1,5 +1,6 @@
 package blog.controller;
 
+import blog.model.Card;
 import blog.service.CardService;
 import blog.service.CreditService;
 import blog.service.PaymentService;
@@ -7,9 +8,11 @@ import blog.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class HomeController {
@@ -61,5 +64,32 @@ public class HomeController {
 	@RequestMapping("/other-payment")
 	public String otherPayment(Model model) {
 		return "other-payment";
+	}
+
+	@RequestMapping(value = "/transfer/{id}", method = RequestMethod.GET)
+	public ModelAndView transfer(@PathVariable Integer id) {
+		ModelAndView modelAndView = new ModelAndView();
+		Card card = cardService.findById(id);
+		modelAndView.addObject("card", card);
+		modelAndView.setViewName("transfer");
+		return modelAndView;
+	}
+
+	@RequestMapping(value = "/transfer", method = RequestMethod.GET)
+	public String transfer(Model model) {
+		Card card = cardService.findById(1);
+		model.addAttribute("card", card);
+		return "transfer";
+	}
+
+	@RequestMapping(value = "/transfer/complete", method = RequestMethod.GET)
+	public ModelAndView complete(RedirectAttributes redirectAttributes) {
+		ModelAndView modelAndView = new ModelAndView();
+		String success = "Средства успешно переведены";
+		redirectAttributes.addFlashAttribute("success", success);
+		modelAndView.addObject("favourites", paymentService.findTop10ByFavouriteIsTrue());
+		modelAndView.addObject("cards", cardService.findAll());
+		modelAndView.setViewName("redirect:/");
+		return modelAndView;
 	}
 }
